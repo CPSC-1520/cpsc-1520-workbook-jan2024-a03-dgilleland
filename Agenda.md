db3d78395078e45eae541ce12d2d5d6d8375f370
Dan Gilleland <dagilleland@shaw.ca>
2024-01-12 09:27:42 -0700

    End of first online class

--- Agenda.md
+++ Agenda.md
@@ -41,11 +41,14 @@
     - Link a JavaScript file
   - Begin [005](./src/005/ReadMe.md)
     - Review [its LOGs](./src/005/LOGs.md)
-  - Practice Assignments
 
 ----
 
 ## Jan 15 (W02)
 
+- [ ] Practice Assignments
+  - About Me
+  - GitDoc Usage
+  - GitDoc Typing Profile
 - [ ] In-Class #1 (**for marks**)
 - *TBA*
---
+++ src/005/intro.js
@@ -0,0 +1,12 @@
+console.clear()
+let unitCost = 12.50
+let quantity = 10
+let total = unitCost * quantity
+total
+total.toFixed(2)
+
+5 + parseInt('7') // should return a number value of 12
+5 + parseFloat('7') // should return a number value of 12
+
+5 + parseInt('7.5') // should return a number value of 12
+5 + parseFloat('7.5') // should return a number value of 12.5
