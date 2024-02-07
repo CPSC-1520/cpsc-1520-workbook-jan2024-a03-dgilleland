47ba64986a95c51ecc4db365efd0495fe0e3c4ff
Dan Gilleland <dgilleland@nait.ca>
2024-02-07 10:36:25 -0700

    Exploring JavaScript Arrays in a Node REPL

---
+++ src/016/JS-Arrays.js
@@ -0,0 +1,36 @@
+let myArray = []; // Declaring a variable and giving it an empty array
+myArray.push(9); // 
+myArray
+myArray.push(7);
+myArray
+myArray[0]
+myArray[2]
+// NOTE: Arrays are zero-based, and the .push() method returns the number of items in the array
+myArray.push(4);
+myArray.length
+console.clear()
+const cls = console.clear
+cls
+cls()
+myArray.pop()
+myArray.push("Coke")
+myArray
+myArray.push(null)
+myArray
+myArray.queue
+myArray.dequeue
+for(var index = 0; index < myArray.length; index++)
+console.log(myArray[index]);
+console.table(myArray)
+cls()
+for(var info of myArray)
+console.log(info)
+for(var info in myArray)
+console.log(info)
+for(var info in myArray)
+console.log(info, typeof info)
+for(var info in myArray)
+console.log(info, typeof info, myArray[info], typeof myArray[info])
+cls()
+for(var info in myArray)
+console.log(info, typeof info, myArray[info], myArray[info] + info)
