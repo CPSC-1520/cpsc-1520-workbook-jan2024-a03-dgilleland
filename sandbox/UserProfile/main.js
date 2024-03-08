833883ecd1979dbd57692a591fa0fbd36bf9f5a4
Dan Gilleland <dgilleland@nait.ca>
2024-03-08 09:12:23 -0700

    Random User - Proof of concept

--- sandbox/UserProfile/main.js
+++ sandbox/UserProfile/main.js
@@ -1,2 +1,25 @@
-import "clam.css";
+import "clam.css"; // Import the Clam CSS framework
 
+// ad-hoc coding
+
+const Person = function(profile) {
+  this.profile = profile;
+}
+
+const assignRandomUserAsync = async function() {
+  let temp;
+  await fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then(data => {
+      temp = new Person(data.results[0]);
+    })
+    .catch(error => console.error(error));
+  return temp;
+}
+
+
+const newUserButton = document.getElementById('new-user');
+newUserButton.addEventListener('click', async function() {
+  let user = await assignRandomUserAsync();
+  console.log(user);
+});
